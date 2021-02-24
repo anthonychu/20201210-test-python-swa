d98cee625f0c5d915af33a9b9c6e9ca61d17611b
Anthony Chu <anthony@anthonychu.ca>
2021-02-23 16:35:25 -0800

    Remove pyodbc

--- api/httptrigger/__init__.py
+++ api/httptrigger/__init__.py
@@ -4,6 +4,6 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    import pyodbc
+    # import pyodbc
     response = requests.get("https://raw.githubusercontent.com/anthonychu/python-func-async/master/host.json")
     return func.HttpResponse(response.text)
